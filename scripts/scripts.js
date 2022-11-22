a1765da50ba6ecfad2577f4f78c402143c909b19
Surafele Asfaw <surafeleasfaw@Surafeles-MacBook-Air.local>
2022-11-22 15:22:31 -0800

    removeevent listeners, change bg color, part of toggling text

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,30 @@
 const clickbutton = document.querySelector('.button1');
-
 function alertFunc() {
-    alert("Hello World!");           
+    alert("Hello World!");     
+    clickbutton.removeEventListener('click', alertFunc)      
   }
 
-clickbutton.addEventListener('click', alertFunc);
+//clickbutton.addEventListener('click', alertFunc, {once:true} <---- works just if you only want it to execute once 
+//clickbutton.removeEventListener('click, alertFunc)
+function changeColor() {
+    // document.body.style.backgroundColor = 'pink'
+    document.body.classList.add('changecolorto')
+};
+
+function buttonText(){
+    clickbutton.textContent = 'Clicked!';
+};
+
+function textToggle(){
+    clickbutton.textContent = 'Click me';
+};
+
+clickbutton.addEventListener('click', changeColor);
+
+if (clickbutton.textContent = 'Clicked!'){
+    clickbutton.addEventListener('click', textToggle)
+}
+else{
+    clickbutton.textContent('click', buttonText)
+};
+
