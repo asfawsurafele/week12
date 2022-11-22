81a8fe4e1cd6394f213ceeef6a2cdc15e6707f21
Surafele Asfaw <surafeleasfaw@Surafeles-MacBook-Air.local>
2022-11-22 15:59:05 -0800

    text content and adding and changing image properties

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,21 +10,31 @@ function changeColor() {
     // document.body.style.backgroundColor = 'pink'
     document.body.classList.add('changecolorto')
 };
-
-function buttonText(){
-    clickbutton.textContent = 'Clicked!';
-};
-
 function textToggle(){
     clickbutton.textContent = 'Click me';
 };
-
-clickbutton.addEventListener('click', changeColor);
-
-if (clickbutton.textContent = 'Clicked!'){
-    clickbutton.addEventListener('click', textToggle)
+function textchecker (){
+if (clickbutton.textContent === 'Click me'){
+    clickbutton.textContent = 'Clicked!'
 }
 else{
-    clickbutton.textContent('click', buttonText)
+    clickbutton.textContent = 'Click me'
+}
 };
+clickbutton.addEventListener('click', changeColor);
+clickbutton.addEventListener('click', textchecker)
+
+// //add another event listener to the button from the previous activity to find the img element with
+// id=“shoppingCart” and update its src, alt, width, and height attribute. Save the image (https://cdn-icons-
+// png.flaticon.com/512/263/263142.png ) in the following folder structure
+
+const sc = document.querySelector('#shoppingCart')
+function sourcechanger (){
+    sc.src = "images/263142.png";
+    sc.alt = "a image of a shopping cart";
+    sc.width = "100";
+    sc.height = "100"
+};
+
+clickbutton.addEventListener('click', sourcechanger)
 
